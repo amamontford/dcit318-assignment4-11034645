77e47ee60568a860ed8e235147ee2615585e61e3
aamontford <amamontford1@gmail.com>
2025-08-23 11:14:32 +0000

    changes

--- PharmacyInventorySystem/obj/Debug/net6.0-windows/PharmacyInventorySystem.AssemblyInfo.cs
+++ PharmacyInventorySystem/obj/Debug/net6.0-windows/PharmacyInventorySystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmacyInventorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d116d2af74e7111b92c30bce4d510b56c80aabd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ce0affb335811afbe6df9f62bdb45affbc78d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmacyInventorySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmacyInventorySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
